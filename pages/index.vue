9358ad7ad489ae096e07feba0749816cf289f65c
IKKO-Ohta <samayotta@gmail.com>
2020-01-03 19:46:06 +0900

    merge pages/index pages/login

--- pages/index.vue
+++ pages/index.vue
@@ -3,44 +3,48 @@
     <v-flex xs12 sm8 md6>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Improve your battle!
         </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em>
-              <small>&mdash; John Leider</small>
-            </em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
+        <v-card-text> {{ hello }}</v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
+          <div v-show="!isLoggedIn" id="firebaseui-auth-container" />
+          <v-btn v-show="isLoggedIn" text small @click="signOut">
+            signOut
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import { Mixins } from 'vue-mixin-decorator'
+import PageMixin from '@/mixins/page-mixins'
+import 'firebaseui/dist/firebaseui.css'
+
+@Component({})
+export default class LoginPage extends Mixins<PageMixin>(PageMixin) {
+  mounted() {
+    this.stores.user.LOGIN()
+    this.stores.sheet.FETCH_SHEET()
+  }
+
+  signOut() {
+    this.stores.user.LOGOUT()
+  }
+
+  get isLoggedIn(): boolean {
+    return this.stores.user.isLogin
+  }
+
+  get hello(): string {
+    if (this.stores.user.currentUserInfo !== null) {
+      return `Hi ${this.stores.user.currentUserInfo.displayName} !`
+    } else {
+      return `Hello!`
+    }
+  }
+}
+</script>
