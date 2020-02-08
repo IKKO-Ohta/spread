0e106182c2e71e51a8b14ea2a5b4e547132366a8
IKKO-Ohta <samayotta@gmail.com>
2020-02-08 21:18:21 +0900

    implement matrix for sp

--- components/performance-by-deck.vue
+++ components/performance-by-deck.vue
@@ -4,13 +4,28 @@
       <v-card-title>デッキ別 勝率集計</v-card-title>
       <v-card-text>
         <v-data-table :headers="headers" :items="items">
-          <template v-slot:body="props">
+          <template v-if="isPC" v-slot:body="props">
             <tr v-for="(item, i) in props.items" :key="i">
               <td v-for="(header, j) in headers" :key="j">
                 <v-chip v-if="shouldColored(header.value)" outlined :ripple="false" :color="getPerformanceColor(item[header.value])">{{ item[header.value] }}</v-chip>
                 <nav v-else>{{ item[header.value] }}</nav>
               </td>
             </tr>
+          </template>
+          <template v-else v-slot:body="props">
+            <tbody>
+              <tr v-for="(item, i) in props.items" :key="i" class="v-data-table__mobile-table-row">
+                <td v-for="(header, j) in headers" :key="j" class="v-data-table__mobile-row">
+                  <div class="v-data-table__mobile-row__header">
+                    {{ header.text }}
+                  </div>
+                  <div class="v-data-table__mobile-row__cell">
+                    <v-chip v-if="header.value !== 'name' && header.value !== 'mirror'" outlined :ripple="false" :color="getPerformanceColor(item[header.value])">{{ item[header.value] }}</v-chip>
+                    <nav v-else>{{ item[header.value] }}</nav>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
           </template>
         </v-data-table>
       </v-card-text>
@@ -22,6 +37,7 @@
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import { GameInfo } from '@/models/@types/game'
 import { Header, VTableRow } from '@/models/@types/matrix'
+import { MAX_SP_WIDTH } from '@/models/const/designs'
 import { PerformanceByDeckHelper } from '@/lib/performance-by-deck-helper'
 import { TestHelper } from '@/lib/test-helper'
 import { PerformanceByDeckHeader } from '@/models/const/performance-by-deck-const'
@@ -37,6 +53,10 @@ export default class PerformanceByDeck extends Vue {
 
   get headers(): Header[] {
     return PerformanceByDeckHeader
+  }
+
+  get isPC(): boolean {
+    return window.innerWidth > MAX_SP_WIDTH
   }
 
   shouldColored(headerVal: string): boolean {
