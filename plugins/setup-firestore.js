aca23eb83e7712b34f0be9ce709e20338e307dba
IKKO-Ohta <samayotta@gmail.com>
2020-03-15 14:23:58 +0900

    modify: use dotenv

--- plugins/setup-firestore.js
+++ plugins/setup-firestore.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import firebasekeyFromEnvVariable from '~/credentials/firebasekey-from-env-variable'
 import firebasekey from '~/credentials/firebasekey'
 
-firebase.initializeApp(firebasekey)
+if (process.env.ci) {
+  firebase.initializeApp(firebasekeyFromEnvVariable)
+} else {
+  firebase.initializeApp(firebasekey)
+}
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
 Vue.prototype.$firestore = firebase.firestore()
